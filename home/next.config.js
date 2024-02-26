b174ff63325857048747dd6fd44d5ba550a88420
Guilherme Gritti <goulart.gritti@outlook.com>
2024-02-26 12:20:35 -0300

    create home mfe and add module federation

---
+++ home/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+module.exports = {
+  webpack(config, options) {
+    const { webpack } = options;
+    Object.assign(config.experiments, { topLevelAwait: true });
+    if (!options.isServer) {
+      //config.cache=false
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "home",
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./screen/Home": "./src/screen/Home.tsx",
+          },
+          shared: {},
+        })
+      );
+    }
+
+    return config;
+  },
+};
---
+++ home/src/pages/index.tsx
@@ -0,0 +1,6 @@
+
+import { Home } from "@/screen/Home";
+
+export default function Default() {
+  return <Home />
+}
